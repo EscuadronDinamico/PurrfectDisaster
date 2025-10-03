ac203dcad09b26b5a2b800cb84ca42e1aa56ffa8
glemerr <pedropabloramirezarenas80@gmail.com>
2025-10-03 15:14:41 -0500

    a

--- Assets/Scripts/Control/Player.cs
+++ Assets/Scripts/Control/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
