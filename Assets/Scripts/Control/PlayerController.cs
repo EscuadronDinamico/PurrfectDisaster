853750fa18e0792d2009a2076d0fdf02588f4f73
glemerr <pedropabloramirezarenas80@gmail.com>
2025-10-20 16:32:00 -0500

    I dont know what im doing D:

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,23 +2,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed;
 
-    private InputControl _playerControl;
-    [SerializeField] float _speed;
+    [SerializeField]
+    private float _jumpSpeed;
+
+    private PlayerInputController _playerInputController;
+    private GroundController _groundController;
+    private Rigidbody _rigidbody;
+    private bool _jumpTriggered;
 
     private void Awake()
     {
-        _playerControl = GetComponent<InputControl>();
-    }
-    private void Update()
-    {
-        Vector3 positionChange = new Vector3(
-            _playerControl.MovementVector.x,
-            0,
-            _playerControl.MovementVector.y)
-            *Time.deltaTime
-            *_speed;
+        _playerInputController = GetComponent<PlayerInputController>();
+        _groundController = GetComponent<GroundController>();
+        _rigidbody = GetComponent<Rigidbody>();
 
-        transform.position += positionChange;
+        _playerInputController.OnJumpButtonPressed += JumpButtonPressed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = new Vector3(
+            _playerInputController.MovementInputVector.x,
+            0,
+            _playerInputController.MovementInputVector.y)
+            * _speed;
+
+        velocity.y = _rigidbody.linearVelocity.y;
+
+        if (_jumpTriggered)
+        {
+            velocity.y = _jumpSpeed;
+            _jumpTriggered = false;
+        }
+
+        _rigidbody.linearVelocity = velocity;
+    }
+
+    private void JumpButtonPressed()
+    {
+        if (_groundController.IsGrounded)
+        {
+            _jumpTriggered = true;
+        }
     }
 }
